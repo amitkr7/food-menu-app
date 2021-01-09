e194f9073d2b0d59db718fd0e13c9fc433c23da2
Amit <amit840901@gmail.com>
2021-01-10 01:16:02 +0530

    Final App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <div className='App'></div>
+  const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((menuItem) => menuItem.category === category)
+    setMenuItems(newItems)
+  }
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
 export default App
